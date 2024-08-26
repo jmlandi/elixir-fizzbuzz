018cc53bb8fc2aa0f817837c085811ac47000ac5
Landi <98327875+jmlandi@users.noreply.github.com>
2024-08-26 10:02:24 -0300

    typo fix

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -2,7 +2,7 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
 
   describe "build/1" do
-    test "when a valid file is provided, retunrs the converted list" do
+    test "when a valid file is provided, returns the converted list" do
       expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, 11, :fizz, 13, 14, :fizzbuzz, 16, 17, :fizz, 19, :buzz]}
       assert FizzBuzz.build("numbers.txt") == expected_response
     end
